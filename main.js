9841b1ae2990a9256e9a08a39692d51bb4d5b93d
Mkamono <chengyaye31@gmail.com>
2022-04-03 21:59:37 +0900

    graphdataの更新処理の実装
    チェックボックスの変更をトリガーとしてインスタンス内のデータを追加/削除できるように実装
    prefectures(県名、コードのリスト)に変数を変更
    cookieにMax-ageを指定

--- main.js
+++ main.js
@@ -2,13 +2,42 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    prefecture: [], //[{prefName, prefCode}]
-    graphdata: [],
+    prefectures: [], //[{prefName, prefCode}]
+    graphdata: [], //[{data, name}]
     APIkey: "",
     chart: null,
   },
   methods: {
     //TODO チェックをトリガーとしてグラフを更新
+    updateGraph: function (event, prefecture) {
+      const self = this;
+      if (event.target.checked) {
+        //TODO 追加時の処理
+        axios
+          .get(
+            `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefecture.prefCode}`,
+            {
+              headers: {
+                "X-API-KEY": self.APIkey,
+              },
+            }
+          )
+          .then(function (res) {
+            let result = res.data.result;
+            let arraydata = []; //調査年、人口数の配列
+            for (let index = 0; index < result.data[0].data.length; index++) {
+              let element = result.data[0].data[index];
+              arraydata.push([element.year, element.value]);
+            }
+            self.graphdata.push({ name: prefecture.prefName, data: arraydata }); //highchartsのseriesと同じデータ構造で追加
+          });
+      } else {
+        //TODO 削除時の処理
+        self.graphdata = self.graphdata.filter(function (element) {
+          return element.name != prefecture.prefName;
+        });
+      }
+    },
   },
   watch: {
     //TODO APIkeyが変更されるたびにRESAS APIにアクセス、県名をゲット
@@ -22,10 +51,10 @@ const vm = new Vue({
         })
         .then(function (res) {
           //サーバーがないとcookieに直接入れるしか方法がわからないが大変良くないことだけは分かる
-          document.cookie = `APIkey=${self.APIkey}; Samesite=strict; Secure`;
+          document.cookie = `APIkey=${self.APIkey}; Samesite=strict; Secure; max-age=3600`;
           let json_pref = res.data.result; //jsonの都道府県名、コード配列
           for (let i = 0; i < json_pref.length; i++) {
-            self.prefecture.push(json_pref[i]);
+            self.prefectures.push(json_pref[i]);
           }
           self.chart = Highcharts.chart("container", {
             // グラフ属性設定
@@ -79,7 +108,6 @@ const vm = new Vue({
     },
   },
   mounted() {
-    //TODO 自動入力処理
     if (document.cookie != "") {
       let arr = new Array(); //cookieを連想配列化
       let tmp = document.cookie.split("; ");
